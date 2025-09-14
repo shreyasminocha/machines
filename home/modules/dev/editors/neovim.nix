65ef43b546970a8dc3fd00eb336d44d0c4e40975
Shreyas Minocha <sm-git@riseup.net>
2025-09-14 03:34:00 -0400

    Move more neovim config into the flake
    Resolves #106

--- home/modules/dev/editors/neovim.nix
+++ home/modules/dev/editors/neovim.nix
@@ -1,6 +1,52 @@
+{ pkgs, ... }:
 {
   programs.neovim = {
     enable = true;
+    plugins = with pkgs.vimPlugins; [
+      {
+        plugin = nvim-treesitter;
+        config = ''
+          lua << EOF
+            require("nvim-treesitter").setup({
+              opts = {
+                ensure_installed = {
+                  "hyprlang",
+                },
+                auto_install = true,
+                highlight = { enable = true },
+              }
+            })
+          EOF
+        '';
+      }
+
+      nvim-tree-lua
+
+      {
+        plugin = lualine-nvim;
+        config = ''
+          lua << EOF
+            require("lualine").setup({
+              options = {
+                disabled_filetypes = {"NvimTree"}
+              }
+            })
+          EOF
+        '';
+      }
+      nvim-web-devicons
+
+      {
+        plugin = nvim-cokeline;
+        config = ''
+          lua << EOF
+            show_if_buffers_are_at_least = 2
+          EOF
+        '';
+      }
+
+      plenary-nvim
+    ];
     extraLuaConfig = ''
       vim.opt.number = true
       vim.opt.linebreak = true
@@ -30,72 +76,30 @@
       vim.opt.showmode = false
 
       vim.keymap.set("", "<C-b>", "<cmd>NvimTreeToggle<cr>", {
-          desc = "Toggle tree"
+        desc = "Toggle tree"
       })
 
       vim.opt.backupdir = os.getenv("HOME") .. "/.local/share/nvim/backup"
 
-      local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-      if not (vim.uv or vim.loop).fs_stat(lazypath) then
-          vim.fn.system(
-              {"git", "clone", "--filter=blob:none", "https://github.com/folke/lazy.nvim.git", "--branch=stable", -- latest stable release
-               lazypath})
-      end
-
-      vim.opt.rtp:prepend(lazypath)
-
-      require("lazy").setup({{
-          "nvim-lualine/lualine.nvim",
-          dependencies = {"nvim-tree/nvim-web-devicons"}
-      }, "nvim-tree/nvim-tree.lua", "marko-cerovac/material.nvim",
-      {
-          "nvim-treesitter/nvim-treesitter",
-          opts = {
-              ensure_installed = {
-                  "hyprlang",
-              },
-              auto_install = true,
-              highlight = { enable = true },
-          }
-      }, {
-          "willothy/nvim-cokeline",
-          dependencies = {"nvim-lua/plenary.nvim", "nvim-tree/nvim-web-devicons"}
-      }}, {})
-
-      require("lualine").setup({
-          options = {
-              disabled_filetypes = {"NvimTree"}
-          }
-      })
-
-      require("nvim-tree").setup()
-
       -- https://github.com/nvim-tree/nvim-tree.lua/wiki/Auto-Close#beauwilliams
       vim.api.nvim_create_autocmd("BufEnter", {
-          group = vim.api.nvim_create_augroup("NvimTreeClose", {
-              clear = true
-          }),
-          pattern = "NvimTree_*",
-          callback = function()
-              local layout = vim.api.nvim_call_function("winlayout", {})
-              if layout[1] == "leaf" and vim.api.nvim_buf_get_option(vim.api.nvim_win_get_buf(layout[2]), "filetype") ==
-                  "NvimTree" and layout[3] == nil then
-                  vim.cmd("confirm quit")
-              end
+        group = vim.api.nvim_create_augroup("NvimTreeClose", {
+          clear = true
+        }),
+        pattern = "NvimTree_*",
+        callback = function()
+          local layout = vim.api.nvim_call_function("winlayout", {})
+          if layout[1] == "leaf" and vim.api.nvim_buf_get_option(vim.api.nvim_win_get_buf(layout[2]), "filetype") ==
+            "NvimTree" and layout[3] == nil then
+            vim.cmd("confirm quit")
           end
+        end
       })
-
-      require("cokeline").setup({
-          show_if_buffers_are_at_least = 2
-      })
-
-      require("material").setup()
-      vim.cmd "colorscheme material"
-      vim.g.material_style = "ocean"
 
       vim.filetype.add({
-          pattern = { [".*/hyprland%.conf"] = "hyprlang" },
+        pattern = { [".*/hyprland%.conf"] = "hyprlang" },
       })
     '';
   };
+  catppuccin.nvim.enable = true;
 }
