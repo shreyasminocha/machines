0270487b93a3a148c02cb79b90f2c4794099ba2e
Shreyas Minocha <sm-git@riseup.net>
2026-01-24 03:41:01 -0500

    [roux] update vaultwarden

--- machines/roux/services/vaultwarden.nix
+++ machines/roux/services/vaultwarden.nix
@@ -17,7 +17,7 @@ in
   #};
 
   virtualisation.oci-containers.containers."vaultwarden" = {
-    image = "vaultwarden/server:1.34.1-alpine";
+    image = "vaultwarden/server:1.35.1-alpine";
     autoStart = true;
     ports = [ "172.17.0.1:${toString local-port}:80" ];
     volumes = [ "/var/lib/vaultwarden:/data" ];
