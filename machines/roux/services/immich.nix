ccab61d861aae543e0e6e10ba36fe58fe4d68fa7
Shreyas Minocha <sm-git@riseup.net>
2026-04-05 19:38:24 -0400

    [roux] replace immich docker with nixos service

--- machines/roux/services/immich.nix
+++ machines/roux/services/immich.nix
@@ -4,36 +4,29 @@
   ...
 }:
 let
-  version = "v2.4.1";
   immich-secret = "services/immich/env";
-  container-redis-socket = "/var/run/redis/redis.sock";
 in
 {
   services.caddy.virtualHosts."pics.minocha.family" = {
     extraConfig = ''
-      reverse_proxy :2283
+      reverse_proxy localhost:2283
     '';
   };
 
-  virtualisation.oci-containers.containers."immich" = {
-    image = "ghcr.io/immich-app/immich-server:${version}";
-    autoStart = true;
-    # ports = [ "8060:2283" ];
-    volumes = [
-      "/home/shreyas/services/immich:/usr/src/app/upload"
-      "/var/run/redis/redis.sock:${container-redis-socket}"
-    ];
-    extraOptions = [
-      "--network=host" # TODO: fix networking mess and remove this
-    ];
-    environment = {
-      IMMICH_VERSION = version;
-      DB_HOSTNAME = "127.0.0.1";
-      DB_DATABASE_NAME = "immich";
-      DB_USERNAME = "immich";
-      REDIS_SOCKET = container-redis-socket;
+  services.immich = {
+    enable = true;
+    port = 2283;
+    database = {
+      user = "immich";
+      name = "immich";
     };
-    environmentFiles = [ config.sops.secrets.${immich-secret}.path ];
+    secretsFile = config.sops.secrets.${immich-secret}.path;
+    mediaLocation = "/var/lib/immich";
+    machine-learning.enable = false; # too weak a machine
+  };
+
+  services.redis.servers.immich = {
+    enable = true;
   };
 
   services.postgresql.extensions = [ pkgs.postgresql15Packages.pgvecto-rs ];
